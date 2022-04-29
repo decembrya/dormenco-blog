34825faf0607e1017568e2cd92195dc53e62e620
Ion Dormenco <idormenco@gmail.com>
2022-04-29 15:15:21 +0300

    change logo and favicon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: 'dormenco.net',
   author: 'Iulia & Ion Dormenco',
-  headerTitle: '',
+  headerTitle: 'dormenco.net',
   description: 'A software development blog from a family of geeks',
   language: 'en-us',
   theme: 'dark', // system, dark or light
